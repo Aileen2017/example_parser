527accd223f80b55f8583c3cd72ecede5049df11
yishu <yishu@YishuDesktop>
2020-06-01 00:48:13 +0100

    Added method_def language and language to call customised method

--- example_parser/src/main/java/com/technoturtle/javacc4/example_parser/expressions/FunctionFactory.java
+++ example_parser/src/main/java/com/technoturtle/javacc4/example_parser/expressions/FunctionFactory.java
@@ -1,13 +1,33 @@
 
-  package com.technoturtle.javacc4.example_parser.expressions; import java.util.*;
+  package com.technoturtle.javacc4.example_parser.expressions; 
+  import java.util.*;
   
-  public class FunctionFactory {
-  
+  public class FunctionFactory implements Expression {
+	  static HashMap<String, FunctionExpression> functionMap = new HashMap<String, FunctionExpression>();
+	  
 	  public static Expression createFunction(String name, List<Expression> parametersList) {
 		  if(name.equals("POW") && parametersList.size()==2)
 			  return new PowerExpression(parametersList.get(0), parametersList.get(1));
+		  else if(functionMap.get(name) != null) {
+			  FunctionExpression functionExpression = functionMap.get(name);
+			  functionExpression.setInputValues(parametersList);
+			  return functionExpression;
+		  }
 		  else
 			  throw new Error("unknown function"+ name);
+	  }
+	  
+	
+	  
+	  
+	  public static void addFunction(FunctionExpression functionDef) {
+		  functionMap.put(functionDef.methodName, functionDef);
+	  }
+	  
+	  public double evaluate(Context context) {
+		 // functionMap.put(functionDef.methodName, functionDef);
+		  return 1.0;
+		  
 	  }
   
 

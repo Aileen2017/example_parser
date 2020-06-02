f1757917c512920cb933669c835f051192917f00
yishu <yishu@YishuDesktop>
2020-06-02 11:46:58 +0100

    Added functionCallExpression to enable multiple calls of the same function in one scope

--- example_parser/src/main/java/com/technoturtle/javacc4/example_parser/expressions/FunctionFactory.java
+++ example_parser/src/main/java/com/technoturtle/javacc4/example_parser/expressions/FunctionFactory.java
@@ -2,16 +2,18 @@
   package com.technoturtle.javacc4.example_parser.expressions; 
   import java.util.*;
   
-  public class FunctionFactory implements Expression {
+  public class FunctionFactory {
 	  static HashMap<String, FunctionExpression> functionMap = new HashMap<String, FunctionExpression>();
 	  
-	  public static Expression createFunction(String name, List<Expression> parametersList) {
+	  
+	  
+	  public static Expression createFunctionCallExpression(String name, List<Expression> parametersList) {
 		  if(name.equals("POW") && parametersList.size()==2)
 			  return new PowerExpression(parametersList.get(0), parametersList.get(1));
 		  else if(functionMap.get(name) != null) {
 			  FunctionExpression functionExpression = functionMap.get(name);
-			  functionExpression.setInputValues(parametersList);
-			  return functionExpression;
+			  FunctionCallExpression functionCallExpression = new FunctionCallExpression(functionExpression, parametersList);	  
+			  return functionCallExpression;
 		  }
 		  else
 			  throw new Error("unknown function"+ name);
@@ -24,11 +26,10 @@
 		  functionMap.put(functionDef.methodName, functionDef);
 	  }
 	  
-	  public double evaluate(Context context) {
-		 // functionMap.put(functionDef.methodName, functionDef);
+	/*  public double evaluate(Context context) {
 		  return 1.0;
 		  
-	  }
+	  }*/
   
 
   
